861c3a24e38a6bad92a32aee7f9053c76660f15b
deepakwadar1909 <deepakwadar192@gmail.com>
2023-07-09 18:09:09 +0530

    Wrapperdemo

---
+++ .history/force-app/main/default/lwc/toastEventDemo/toastEventDemo_20230709175118.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+export default class ToastEventDemo extends LightningElement {
+
+    myTitle = 'Salesforce Toast Event'
+
+    //1st fucntion
+    
+}
---
+++ .history/force-app/main/default/lwc/toastEventDemo/toastEventDemo_20230709175500.js
@@ -0,0 +1,23 @@
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+export default class ToastEventDemo extends LightningElement {
+
+    myTitle = 'Salesforce Toast Event'
+
+    //1st function
+    handleClick(){
+
+    }
+
+    //2nd function
+
+    showToast(){
+        const event = new ShowToastEvent({
+            title : 'Show Toast Demo',
+            message : 'Toast Example Demo',
+            variant : 'success',
+        })
+        this.dispatchEvent(event);
+    }
+
+}
